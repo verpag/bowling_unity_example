9dc1dd117f3d38ba3966a73d9640fe5dd93340e0
verpag <btok900@gmail.com>
2019-11-15 17:20:26 +0300

    title scene and assets
    implemented title scene and put assets to folder

---
+++ bowling-unity-example/Assets/PanelScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            SceneManager.LoadScene("GameScene");
+    }
+}
